fc8e0b1ea9ec5b77e63a0ae65abab9d8d5b482ee
Martin Vaaden <mavaaden@gmail.com>
2015-12-17 21:50:12 +0100

    Removed password for testuser (and changed password, he's only a local dude with limited rights :) )

--- test/mantest.js
+++ test/mantest.js
@@ -1,24 +1,29 @@
-var expect = require("chai").expect;
+var util = require('util');
+var vTestConnection = require("../main");
 
-var vTestConnection = require("../main"), vTestConnection;
+console.log("Password: ");
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-expect(vTestConnection).to.be.an("object");
+process.stdin.on('data', function (text) {
+	vPasswd = text.replace(/(\r\n|\n|\r)/gm,"");
 
-vTestConnection.Connect({
-	host     : "127.0.0.1",
-	port     : "3307",
-	user     : "testuser",
-	password : "Ar4bi4",
-	database : "dev_useraccess"
-}, function(pStatus){
-	expect(pStatus).to.equal("success");
-
-	vTestConnection.Select("secv_myuser", "*", function(pRows, pFields){
-		pRows.forEach(function(pRow, pRowIndex){
-			pFields.forEach(function(pColumn, pColumnIndex){
-				console.log(pColumn.name + ": " + pRow[pColumn.name]);
-			});
-		})
-		vTestConnection.Close();
+	vTestConnection.Connect({
+		host     : "127.0.0.1",
+		port     : "3307",
+		user     : "testuser",
+		password : vPasswd,
+		database : "dev_useraccess"
+	}, function(pStatus){
+		vTestConnection.Select("secv_myuser", "*", function(pRows, pFields){
+			pRows.forEach(function(pRow, pRowIndex){
+				pFields.forEach(function(pColumn, pColumnIndex){
+					console.log(pColumn.name + ": " + pRow[pColumn.name]);
+				});
+			})
+			vTestConnection.Close();
+			process.exit();
+		});
 	});
+
 });
